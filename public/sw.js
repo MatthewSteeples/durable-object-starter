1a49bfac62082836a5d2f78541220bb2cd7daaa3
Matthew Steeples <matthew@ledgerscope.com>
2025-11-22 02:16:34 +0000

    Add sample push message handler

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,16 @@
-// Minimal service worker for push notifications
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.text() : {};
-  const title = data.title || 'Notification';
-  const options = {
-    body: data,
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
+// Listen to push events.
+self.addEventListener('push', event => {
+  // Check if the user has granted permission to display notifications.
+  if (Notification.permission === "granted") {
+    // Get the notification data from the server.
+    const notificationText = event.data.text();
+
+    // Display a notification.
+    const showNotificationPromise = self.registration.showNotification('Sample Push', {
+      body: notificationText
+    });
+
+    // Keep the service worker running until the notification is displayed.
+    event.waitUntil(showNotificationPromise);
+  }
 });
